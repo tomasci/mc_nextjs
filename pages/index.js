4e76f73742a2ce173b51974eaa6012845361921e
tomasci <zivgta@gmail.com>
2021-08-28 23:17:43 +0300

    action finally works

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,32 @@
-// import Head from 'next/head'
-// // import Image from 'next/image'
-// import IndexPosts from "../src/components/Posts/IndexPosts"
-// import makeApiUrl from "../src/api"
-// import Layout from "../src/components/layout"
-//
-// export default function Home({postsData}) {
-//     return (
-//         <Layout>
-//             <Head>
-//                 <title>
-//                     MinecraftMods.xyz – Minecraft Bedrock & Minecraft PE mods database
-//                 </title>
-//                 <meta name={"description"} content={"MinecraftMods.xyz – is the Minecraft Bedrock and Minecraft PE mods database"}/>
-//             </Head>
-//             <div className={"content"}>
-//                 <IndexPosts postsData={postsData} currentPage={1}/>
-//             </div>
-//         </Layout>
-//     )
-// }
-//
-// export async function getServerSideProps() {
-//     const posts = await fetch(makeApiUrl('/mods/page/1'))
-//     const postsData = await posts.json()
-//
-//     return {
-//         props: {
-//             postsData
-//         }
-//     }
-// }
+import Head from 'next/head'
+// import Image from 'next/image'
+import IndexPosts from "../src/components/Posts/IndexPosts"
+import makeApiUrl from "../src/api"
+import Layout from "../src/components/layout"
 
-function Home() {
+export default function Home({postsData}) {
     return (
-        <>
-            hello world // ga {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} // au {process.env.NEXT_PUBLIC_API_URL}
-        </>
+        <Layout>
+            <Head>
+                <title>
+                    MinecraftMods.xyz – Minecraft Bedrock & Minecraft PE mods database
+                </title>
+                <meta name={"description"} content={"MinecraftMods.xyz – is the Minecraft Bedrock and Minecraft PE mods database"}/>
+            </Head>
+            <div className={"content"}>
+                <IndexPosts postsData={postsData} currentPage={1}/>
+            </div>
+        </Layout>
     )
 }
 
-export default Home
+export async function getServerSideProps() {
+    const posts = await fetch(makeApiUrl('/mods/page/1'))
+    const postsData = await posts.json()
+
+    return {
+        props: {
+            postsData
+        }
+    }
+}
