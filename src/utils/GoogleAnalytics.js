92a9b27b3761a9acd3b3b0da47f9f3bb6238e0d1
tomasci <zivgta@gmail.com>
2021-09-07 22:20:05 +0300

    YM update

--- src/utils/GoogleAnalytics.js
+++ src/utils/GoogleAnalytics.js
@@ -14,8 +14,7 @@ export const event = ({ action, params }) => {
 	// ym(process.env.NEXT_PUBLIC_YANDEX_METRIKA, 'reachGoal', action.toString(), params)
 	// https://api.minecraftmods.xyz/uploads/source/353cdce6-172f-490f-bbe0-fb40f126cebb.mcpack
 
-	let fileUrl = process.env.NEXT_PUBLIC_API_URL + '/uploads/source/' + params.packPath + '.mcpack'
-	ym(process.env.NEXT_PUBLIC_YANDEX_METRIKA, 'file', fileUrl, {
-		referer: process.env.NEXT_PUBLIC_SITE_URL + '/read/' + params.packID
-	})
+	// let fileUrl = process.env.NEXT_PUBLIC_API_URL + '/uploads/source/' + params.packPath + '.mcpack'
+	let refererUrl = process.env.NEXT_PUBLIC_SITE_URL + '/read/' + params.packID
+	ym(process.env.NEXT_PUBLIC_YANDEX_METRIKA, 'file', refererUrl)
 }
